7130989ff7611eeff9dc7009ab2f2e88f4a51ddd
Keri Deemer <kerid18@att.net>
2019-09-16 01:15:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   original_array = string.split("")
   new_string = []
-  original_string.each do |string|
+  original_array.each do |string|
     new_string << string.reverse
   end
   new_string.join("")
